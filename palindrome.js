4c652688c516669343d10a82a5bef63b1dbd8400
harsh7739 <hrbhumi123@gmail.com>
2022-11-06 00:39:08 +0530

    prime and palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+let str = "madam";
+let str1 = "";
+for(let i=0;i>=str.length-1;i++){
+     str1+=srt[i];
+}
+  if(str==str1){
+     console.log("it is palindrome");
+   }
+     else {
+       console.log("Not palindrome");
--- prime.js
+++ prime.js
@@ -1,1 +1,51 @@
-console.log("hello prime")
+  let num = 13;
+   for(let i=num;i<=i;i++){
+     if(num%i==0)
+       count++;
+   }
+  }
+     if(count==2){
+       console.log("No. is prime");
+    } else {
+        console.log("No. is not prime");
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+5
+
+
+
+
+
+
+
+
+
+
+
+
+5
+
+    
